354ef2fdc5700a3f3c7a0ec3582b5d4050eef2ab
Hazem <reach.hazim@gmail.com>
2021-09-26 16:01:30 +0200

    update env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   env: {
-    API_URL: process.env.API_URL,
+    API_URL: process.env?.API_URL,
   },
   reactStrictMode: true,
   async rewrites() {
